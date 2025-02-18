5d8fcf1e6c781bbef998f750c42d27a98c1bda08
muluthfi <luthfi.wongpati@gmail.com>
2025-02-18 14:52:47 +0700

    add about js

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+export default function About() {
+    return (
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+            <div className="bg-white p-8 rounded-lg shadow-lg text-center">
+                <h1 className="text-3xl font-bold text-gray-700">About Us</h1>
+                <p className="text-gray-500 my-4">This is the about page</p>
+            </div>
+        </div>
+    )
+
+}
