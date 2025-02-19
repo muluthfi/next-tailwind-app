f96e845cff3a803ea09ed8520dcaa70a1eb67793
muluthfi <luthfi.wongpati@gmail.com>
2025-02-19 10:45:58 +0700

    add token bearer

--- pages/transaction.js
+++ pages/transaction.js
@@ -4,29 +4,23 @@ import { useState } from 'react';
 import { redirect } from 'next/dist/server/api-utils';
 
 export async function getServerSideProps() {
-    const response = await fetch("http://localhost:3000/transaction");
-    const headers = response.headers;
+    const response = await fetch("http://localhost:3000/transaction", { headers: {
+        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imx1dGhmaSIsImlhdCI6MTczOTkzNTc4MywiZXhwIjoxNzM5OTM5MzgzfQ.FPY-hjQQsWcwJRTVwgqVxgbuJ1XLM2SI6RonA2AdDUc'
+    }});
     const datas = await response.json();
-    if(headers!=200){
-        return {
-            error: {
-                status: headers,
-                message: headers.message,
-            }
-        }
-    }
-
+    const status = await response.status;
     return {
         props: {
             get: datas,
+            status: status
         },
     }    
 }
 
-export default function Transaction({ get }) {
+export default function Transaction({ get,status }) {
     return (
         <div className="container mx-auto p-4">
-            <h1 className="text-2xl font-bold mb-4 transition-opacity duration-700 ease-in opacity-100 hover:opacity-0">Transactions</h1>
+            <h1 className="text-2xl font-bold mb-4 transition-opacity duration-700 ease-in opacity-100 hover:opacity-0 text-center">Transactions</h1>
             <table className="min-w-full border-collapse border-black transition-opacity duration-700 ease-in opacity-100 hover:opacity-0">
                 <thead>
                     <tr>
@@ -36,13 +30,17 @@ export default function Transaction({ get }) {
                     </tr>
                 </thead>
                 <tbody>
-                    {get.map((data) => (
+                    {status!==200 && <tr><td colSpan="3">Unauthorized</td></tr>}
+                    {status === 200 &&
+                    get.map((data) => (
                         <tr key={data._id}>
-                            <td className="border px-4 py-2 text-center">{data.date}</td>
+                            <td className="border px-4 py-2 text-center">{new Date(data.date).toLocaleDateString()}</td>
                             <td className="border px-4 py-2 text-center">{data.amount}</td>
                             <td className="border px-4 py-2 text-center">{data.description}</td>
                         </tr>
-                    ))}
+                    ))
+                    }
+                    
                 </tbody>
             </table>
         </div>
